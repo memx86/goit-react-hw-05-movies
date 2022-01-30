af053e197aad553a611c93d6a65d6d5e38a89759
Yevhen Malyshko <memx86@gmail.com>
2022-01-30 13:05:04 +0200

    chunks lazy loading

--- src/App.js
+++ src/App.js
@@ -1,28 +1,42 @@
-import { Fragment } from "react";
+import { Fragment, lazy, Suspense } from "react";
 import Navbar from "components/Navbar";
 import { Route, Routes } from "react-router-dom";
-import Home from "views/Home";
-import Movies from "views/Movies";
-import MovieCard from "components/MovieCard";
-import Cast from "components/Cast";
-import Reviews from "components/Reviews";
-import NotFound from "views/NotFound";
+import Loader from "components/Loader";
+// import Home from "views/Home";
+// import Movies from "views/Movies";
+// import MovieCard from "components/MovieCard";
+// import Cast from "components/Cast";
+// import Reviews from "components/Reviews";
 
+const Home = lazy(() => import("./views/Home" /*webpackChunkName: "Home"*/));
+const Movies = lazy(() =>
+  import("./views/Movies" /*webpackChunkName: "Movies"*/)
+);
+const MovieCard = lazy(() =>
+  import("./components/MovieCard" /*webpackChunkName: "MovieCard"*/)
+);
+const Cast = lazy(() =>
+  import("./components/Cast" /*webpackChunkName: "Cast"*/)
+);
+const Reviews = lazy(() =>
+  import("./components/Reviews" /*webpackChunkName: "Reviews"*/)
+);
 function App() {
   return (
     <Fragment>
       <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/movies" element={<Movies />} />
-        <Route path="/movies/:movieId" element={<MovieCard />}>
-          <Route index element={<div></div>} />
-          <Route path="cast" element={<Cast />} />
-          <Route path="reviews" element={<Reviews />} />
-        </Route>
-        <Route path="*" element={<Home />} />
-        {/* <Route path="*" element={<NotFound />} /> */}
-      </Routes>
+      <Suspense fallback={<Loader />}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/movies" element={<Movies />} />
+          <Route path="/movies/:movieId" element={<MovieCard />}>
+            <Route index element={<div></div>} />
+            <Route path="cast" element={<Cast />} />
+            <Route path="reviews" element={<Reviews />} />
+          </Route>
+          <Route path="*" element={<Home />} />
+        </Routes>
+      </Suspense>
     </Fragment>
   );
 }
