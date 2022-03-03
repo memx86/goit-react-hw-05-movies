50811c03e82a8757be2835f833ca75c1d8a79580
Yevhen Malyshko <memx86@gmail.com>
2022-03-03 09:55:51 +0200

    use query for movie search

--- src/components/MovieSearch/MovieSearch.js
+++ src/components/MovieSearch/MovieSearch.js
@@ -1,56 +1,23 @@
-import { useEffect, useState } from "react";
+import { useQuery } from "react-query";
 import { useLocation, useSearchParams } from "react-router-dom";
 import MovieList from "components/MovieList";
 import Loader from "components/Loader";
 import * as movieAPI from "../../js/moviesAPI";
 // import s from "./MovieSearch.module.css";
 
-function MovieSearch(props) {
+function MovieSearch() {
   const [searchParams] = useSearchParams();
-  const [movies, setMovies] = useState([]);
-  const [loader, setLoader] = useState(false);
-  const { pathname } = useLocation();
   const query = searchParams.get("query");
-  useEffect(() => {
-    if (!query) {
-      setMovies([]);
-      return;
-    }
-    setLoader(true);
-    movieAPI
-      .getMovies(query)
-      .then(setMovies)
-      .finally(() => setLoader(false));
-  }, [query]);
 
-  if (loader) return <Loader />;
-  return <MovieList movies={movies} pathname={pathname} />;
+  const { data, isLoading } = useQuery(["movies", query], getMovies);
+  function getMovies() {
+    if (!query) return [];
+    return movieAPI.getMovies(query);
+  }
+  const { pathname } = useLocation();
+
+  if (isLoading) return <Loader />;
+  return <MovieList movies={data} pathname={pathname} />;
 }
 
 export default MovieSearch;
-
-// import { useQuery } from "react-query";
-// import { useLocation, useSearchParams } from "react-router-dom";
-// import MovieList from "components/MovieList";
-// import Loader from "components/Loader";
-// import * as movieAPI from "../../js/moviesAPI";
-// // import s from "./MovieSearch.module.css";
-
-// function MovieSearch(props) {
-//   const [searchParams] = useSearchParams();
-//   const query = searchParams.get("query");
-
-//   console.log("query", query);
-//   const { data, isLoading } = useQuery("movies", getMovies);
-//   function getMovies() {
-//     if (!query) return [];
-//     console.log("fetch");
-//     return movieAPI.getMovies(query);
-//   }
-//   const { pathname } = useLocation();
-
-//   if (isLoading) return <Loader />;
-//   return <MovieList movies={data} pathname={pathname} />;
-// }
-
-// export default MovieSearch;
